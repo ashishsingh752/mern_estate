06f8bc981c45e12ed5ead284af3154949736b4c0
Ashish <theashish752@gmail.com>
2023-12-12 17:28:08 +0530

    Create Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
-import About from "./pages/About"
-import Profile from "./pages/Profile"
-import Home from "./pages/Home"
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
+import Header from "./components/Header"
 
 export default function App() {
-  return<BrowserRouter>
-        <Routes>
-            <Route path="/sign-in" element={<SignIn/>}></Route>
-            <Route path="/sign-up" element={<SignUp/>}></Route>
-            <Route path="/" element={<Home/>}></Route>
-            <Route path="/about" element={<About/>}></Route>
-            <Route path="/profile" element={<Profile/>}></Route>
-        </Routes>
+  return (
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+      </Routes>
     </BrowserRouter>
-  
+  );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className="bg-slate-200 shadow-md ">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <h1 className="flex  flex-wrap font-bold text-sm sm:text-xl">
+          <span className="text-slate-700 font-semibold">Ashish</span>
+          <span className="text-slate-500 font-semibold">Singh</span>
+        </h1>
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center ">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
+        <ul className="flex gap-4 ">
+          <Link to="/">
+            <li className=" text-slate-700 hidden sm:inline hover:underline">Home</li>
+          </Link>
+
+          <Link to="/About">
+            <li className="text-slate-700 hidden sm:inline hover:underline">About</li>
+          </Link>
+
+          <Link to="/sign-in">
+            <li className="text-slate-700 hover:underline">SignIn</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
