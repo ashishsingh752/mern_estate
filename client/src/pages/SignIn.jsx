c65e197d1b8555df33a0551b1d9f59dde797145c
Ashish <theashish752@gmail.com>
2023-12-13 19:08:35 +0530

    complete signIn and singUp UI

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function SignIn() {
   return (
-    <div>
-      SignIn
+    <div className="mx-auto max-w-lg">
+      <h1 className="text-3xl text-center font-semibold my-7 ">Sign in</h1>
+      <form className="flex  flex-col gap-4 ">
+        <input
+          type="email"
+          placeholder="Email"
+          className=" p-3 rounded-lg"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className=" p-3 rounded-lg"
+          id="password"
+        />
+        <button className="bg-slate-600 p-3 rounded-lg uppercase text-white hover:opacity-90 disabled:opacity-70 ">
+          sign in
+        </button>
+      </form>
+
+      <div className="flex mt-4 gap-2">
+      <p>
+       Don't Have an account 
+      </p>
+        <Link to={'/sign-up'} >
+          <span className=" text-blue-700"> sign up</span>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
   return (
-    <div>
-      SignUp
+    <div className="mx-auto max-w-lg">
+      <h1 className="text-3xl text-center font-semibold my-7 ">Sign up</h1>
+      <form className="flex  flex-col gap-4 ">
+        <input
+          type="text"
+          placeholder="Username"
+          className=" p-3 rounded-lg"
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className=" p-3 rounded-lg"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className=" p-3 rounded-lg"
+          id="password"
+        />
+        <button className="bg-slate-600 p-3 rounded-lg uppercase text-white hover:opacity-90 disabled:opacity-70 ">
+          signup
+        </button>
+      </form>
+      <div className="flex mt-4 gap-2">
+      <p>
+        have an account 
+      </p>
+        <Link to={'/sign-in'} >
+          <span className=" text-blue-700"> sign in</span>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
