953385023983e8d7133528d418a76b3004be6732
Ashish <theashish752@gmail.com>
2023-12-18 19:56:02 +0530

    completed home page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,8 +27,8 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Book</span>
-            <span className='text-slate-700'>Hotel</span>
+            <span className='text-slate-500'>My</span>
+            <span className='text-slate-700'>Home</span>
           </h1>
         </Link>
         <form
